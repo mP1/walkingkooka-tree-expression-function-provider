fbbbff24ec4e04f58c821284536bf10d59f5e533
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2024-07-05 08:35:23 +1000

    TreeExpressionFunctionProvider
    - Closes https://github.com/mP1/walkingkooka-tree-expression-function-provider/issues/69
    - TreeExpressionFunctionProvider

--- src/main/java/walkingkooka/tree/expression/function/provider/ExpressionFunctionProviders.java
+++ src/main/java/walkingkooka/tree/expression/function/provider/ExpressionFunctionProviders.java
@@ -18,6 +18,7 @@
 package walkingkooka.tree.expression.function.provider;
 
 import walkingkooka.net.AbsoluteUrl;
+import walkingkooka.net.Url;
 import walkingkooka.reflect.PublicStaticHelper;
 import walkingkooka.text.CaseSensitivity;
 import walkingkooka.tree.expression.ExpressionEvaluationContext;
@@ -26,6 +27,14 @@ import walkingkooka.tree.expression.function.ExpressionFunction;
 import java.util.Set;
 
 public final class ExpressionFunctionProviders implements PublicStaticHelper {
+
+    /**
+     * This is the base {@link AbsoluteUrl} for all {@link ExpressionFunction} in this package. The name of each
+     * function will be appended to this base.
+     */
+    public final static AbsoluteUrl BASE_URL = Url.parseAbsolute(
+            "https://github.com/mP1/walkingkooka-tree-expression-function-provider/" + ExpressionFunction.class.getSimpleName()
+    );
 
     /**
      * {@see BasicExpressionFunctionProvider}
@@ -52,6 +61,13 @@ public final class ExpressionFunctionProviders implements PublicStaticHelper {
      */
     public static ExpressionFunctionProvider empty() {
         return EmptyExpressionFunctionProvider.INSTANCE;
+    }
+
+    /**
+     * {@see TreeExpressionFunctionProvider}
+     */
+    public static ExpressionFunctionProvider expressionFunctions() {
+        return TreeExpressionFunctionProvider.INSTANCE;
     }
 
     /**
