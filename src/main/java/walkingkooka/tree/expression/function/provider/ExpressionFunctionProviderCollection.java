f266bc1d70aa869dacfe00b6a7f88413d7be05e3
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2024-07-12 14:07:44 +1000

    https://github.com/mP1/walkingkooka-plugin/pull/113 ProviderCollection.get(PluginSelectorLike)
    - https://github.com/mP1/walkingkooka-plugin/pull/113
    - ProviderCollection.get(PluginSelectorLike)
    
    - https://github.com/mP1/walkingkooka-plugin/pull/114
    - ProviderCollection input-to-name function removed

--- src/main/java/walkingkooka/tree/expression/function/provider/ExpressionFunctionProviderCollection.java
+++ src/main/java/walkingkooka/tree/expression/function/provider/ExpressionFunctionProviderCollection.java
@@ -17,16 +17,19 @@
 
 package walkingkooka.tree.expression.function.provider;
 
+import walkingkooka.Cast;
 import walkingkooka.collect.list.Lists;
+import walkingkooka.plugin.PluginSelectorLike;
 import walkingkooka.plugin.ProviderCollection;
+import walkingkooka.plugin.ProviderCollectionProviderGetter;
 import walkingkooka.tree.expression.ExpressionEvaluationContext;
 import walkingkooka.tree.expression.FunctionExpressionName;
 import walkingkooka.tree.expression.function.ExpressionFunction;
 
+import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
 import java.util.Set;
-import java.util.function.Function;
 
 /**
  * A {@link ExpressionFunctionProvider} view of a collection of {@link ExpressionFunctionProvider providers}.
@@ -41,8 +44,24 @@ final class ExpressionFunctionProviderCollection implements ExpressionFunctionPr
 
     private ExpressionFunctionProviderCollection(final Set<ExpressionFunctionProvider> providers) {
         this.providers = ProviderCollection.with(
-                Function.identity(), // inputToName
-                (p, n, v) -> p.expressionFunction(n),
+                new ProviderCollectionProviderGetter<>() {
+                    @Override
+                    public Optional<ExpressionFunction<?, ?>> get(final ExpressionFunctionProvider provider,
+                                                                  final FunctionExpressionName name,
+                                                                  final List<?> values) {
+                        return Cast.to(
+                                provider.expressionFunction(
+                                        name
+                                )
+                        );
+                    }
+
+                    @Override
+                    public Optional<ExpressionFunction<?, ?>> get(final ExpressionFunctionProvider provider,
+                                                                  final PluginSelectorLike<FunctionExpressionName> selector) {
+                        throw new UnsupportedOperationException();
+                    }
+                },
                 ExpressionFunctionProvider::expressionFunctionInfos,
                 ExpressionFunction.class.getSimpleName(),
                 providers
@@ -53,9 +72,11 @@ final class ExpressionFunctionProviderCollection implements ExpressionFunctionPr
     public Optional<ExpressionFunction<?, ExpressionEvaluationContext>> expressionFunction(final FunctionExpressionName name) {
         Objects.requireNonNull(name, "name");
 
-        return this.providers.get(
-                name,
-                Lists.empty()
+        return Cast.to(
+                this.providers.get(
+                        name,
+                        Lists.empty()
+                )
         );
     }
 
@@ -64,9 +85,7 @@ final class ExpressionFunctionProviderCollection implements ExpressionFunctionPr
         return this.providers.infos();
     }
 
-    private final ProviderCollection<FunctionExpressionName, ExpressionFunctionInfo, ExpressionFunctionProvider,
-            FunctionExpressionName,
-            ExpressionFunction<?, ExpressionEvaluationContext>> providers;
+    private final ProviderCollection<ExpressionFunctionProvider, FunctionExpressionName, ExpressionFunctionInfo, PluginSelectorLike<FunctionExpressionName>, ExpressionFunction<?, ?>> providers;
 
     @Override
     public String toString() {
