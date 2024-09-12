b94ea9c1ed3ca743c460ea251f4122c54a1be1b2
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2024-09-12 11:59:25 +1000

    FilteredExpressionFunctionProvider

---
+++ src/main/java/walkingkooka/tree/expression/function/provider/FilteredExpressionFunctionProvider.java
@@ -0,0 +1,80 @@
+/*
+ * Copyright 2024 Miroslav Pokorny (github.com/mP1)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package walkingkooka.tree.expression.function.provider;
+
+import walkingkooka.plugin.ProviderContext;
+import walkingkooka.tree.expression.ExpressionEvaluationContext;
+import walkingkooka.tree.expression.ExpressionFunctionName;
+import walkingkooka.tree.expression.function.ExpressionFunction;
+
+import java.util.List;
+import java.util.Objects;
+
+/**
+ * A {@link ExpressionFunctionProvider} that provides functions from one provider but lists more {@link ExpressionFunctionInfo}.
+ */
+final class FilteredExpressionFunctionProvider implements ExpressionFunctionProvider {
+
+    static FilteredExpressionFunctionProvider with(final ExpressionFunctionProvider provider,
+                                                   final ExpressionFunctionInfoSet infos) {
+        return new FilteredExpressionFunctionProvider(
+                Objects.requireNonNull(provider, "provider"),
+                Objects.requireNonNull(infos, "infos")
+        );
+    }
+
+    private FilteredExpressionFunctionProvider(final ExpressionFunctionProvider provider,
+                                              final ExpressionFunctionInfoSet infos) {
+        this.provider = provider;
+        this.infos = infos;
+    }
+
+    @Override
+    public ExpressionFunction<?, ExpressionEvaluationContext> expressionFunction(final ExpressionFunctionSelector selector,
+                                                                                 final ProviderContext context) {
+        return this.provider.expressionFunction(
+                selector,
+                context
+        );
+    }
+
+    @Override
+    public ExpressionFunction<?, ExpressionEvaluationContext> expressionFunction(final ExpressionFunctionName name,
+                                                                                 final List<?> values,
+                                                                                 final ProviderContext context) {
+        return this.provider.expressionFunction(
+                name,
+                values,
+                context
+        );
+    }
+
+    private final ExpressionFunctionProvider provider;
+
+    @Override
+    public ExpressionFunctionInfoSet expressionFunctionInfos() {
+        return this.infos;
+    }
+
+    private final ExpressionFunctionInfoSet infos;
+
+    @Override
+    public String toString() {
+        return this.provider.toString();
+    }
+}
