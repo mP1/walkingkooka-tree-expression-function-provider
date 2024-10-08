1068ff77c9dc13314ff735c29a6e81a6a2f80c6b
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2024-10-08 18:10:54 +1100

    https://github.com/mP1/walkingkooka-plugin/pull/317 PluginAliasSet PluginAliasLike replaces PluginAlias
    - https://github.com/mP1/walkingkooka-plugin/pull/317
    = PluginAliasSet PluginAliasLike replaces PluginAlias

--- src/main/java/walkingkooka/tree/expression/function/provider/ExpressionFunctionAliases.java
+++ src/main/java/walkingkooka/tree/expression/function/provider/ExpressionFunctionAliases.java
@@ -17,24 +17,55 @@
 
 package walkingkooka.tree.expression.function.provider;
 
-import walkingkooka.Cast;
+import org.junit.jupiter.api.Test;
+import walkingkooka.collect.set.ImmutableSortedSetDefaults;
+import walkingkooka.collect.set.SortedSets;
 import walkingkooka.plugin.PluginAliasSet;
-import walkingkooka.plugin.PluginAliasesLike;
+import walkingkooka.plugin.PluginAliasSetLike;
 import walkingkooka.text.CharacterConstant;
+import walkingkooka.text.printer.IndentingPrinter;
 import walkingkooka.tree.expression.ExpressionFunctionName;
 import walkingkooka.tree.json.JsonNode;
 import walkingkooka.tree.json.marshall.JsonNodeContext;
 import walkingkooka.tree.json.marshall.JsonNodeMarshallContext;
 import walkingkooka.tree.json.marshall.JsonNodeUnmarshallContext;
 
+import java.util.AbstractSet;
+import java.util.Comparator;
+import java.util.Iterator;
 import java.util.Optional;
+import java.util.SortedSet;
 
-public final class ExpressionFunctionAliases implements PluginAliasesLike<ExpressionFunctionName, ExpressionFunctionInfo, ExpressionFunctionInfoSet, ExpressionFunctionSelector> {
+public final class ExpressionFunctionAliases extends AbstractSet<ExpressionFunctionAlias>
+        implements PluginAliasSetLike<ExpressionFunctionName,
+        ExpressionFunctionInfo,
+        ExpressionFunctionInfoSet,
+        ExpressionFunctionSelector,
+        ExpressionFunctionAlias>,
+        ImmutableSortedSetDefaults<ExpressionFunctionAliases, ExpressionFunctionAlias> {
+
+    /**
+     * An empty {@link ExpressionFunctionAliases}.
+     */
+    public final static ExpressionFunctionAliases EMPTY = new ExpressionFunctionAliases(
+            PluginAliasSet.with(
+                    SortedSets.empty(),
+                    ExpressionFunctionPluginHelper.INSTANCE
+            )
+    );
 
     /**
      * {@see PluginAliasSet#SEPARATOR}
      */
     public final static CharacterConstant SEPARATOR = PluginAliasSet.SEPARATOR;
+
+    /**
+     * Factory that creates {@link ExpressionFunctionAliases} with the given aliases.
+     */
+    @Test
+    public static ExpressionFunctionAliases with(final SortedSet<ExpressionFunctionAlias> aliases) {
+        return EMPTY.setElements(aliases);
+    }
 
     public static ExpressionFunctionAliases parse(final String text) {
         return new ExpressionFunctionAliases(
@@ -64,25 +95,81 @@ public final class ExpressionFunctionAliases implements PluginAliasesLike<Expres
         return this.pluginAliasSet.merge(infos);
     }
 
-    // Object...........................................................................................................
+    // ImmutableSortedSet...............................................................................................
 
     @Override
-    public int hashCode() {
-        return this.pluginAliasSet.hashCode();
+    public Comparator<? super ExpressionFunctionAlias> comparator() {
+        return this.pluginAliasSet.comparator();
     }
 
     @Override
-    public boolean equals(Object other) {
-        return this == other || other instanceof ExpressionFunctionAliases && this.equals0(Cast.to(other));
-    }
-
-    private boolean equals0(final ExpressionFunctionAliases other) {
-        return this.pluginAliasSet.equals(other.pluginAliasSet);
+    public Iterator<ExpressionFunctionAlias> iterator() {
+        return this.pluginAliasSet.stream().iterator();
     }
 
     @Override
-    public String toString() {
+    public int size() {
+        return this.pluginAliasSet.size();
+    }
+
+    @Override
+    public ExpressionFunctionAliases setElements(final SortedSet<ExpressionFunctionAlias> aliases) {
+        final ExpressionFunctionAliases after = new ExpressionFunctionAliases(
+                this.pluginAliasSet.setElements(aliases)
+        );
+        return this.pluginAliasSet.equals(aliases) ?
+                this :
+                after;
+    }
+
+    @Override
+    public SortedSet<ExpressionFunctionAlias> toSet() {
+        return this.pluginAliasSet.toSet();
+    }
+
+    @Override
+    public ExpressionFunctionAliases subSet(final ExpressionFunctionAlias from,
+                                            final ExpressionFunctionAlias to) {
+        return this.setElements(
+                this.pluginAliasSet.subSet(
+                        from,
+                        to
+                )
+        );
+    }
+
+    @Override
+    public ExpressionFunctionAliases headSet(final ExpressionFunctionAlias alias) {
+        return this.setElements(
+                this.pluginAliasSet.headSet(alias)
+        );
+    }
+
+    @Override
+    public SortedSet<ExpressionFunctionAlias> tailSet(final ExpressionFunctionAlias alias) {
+        return this.setElements(
+                this.pluginAliasSet.tailSet(alias)
+        );
+    }
+
+    @Override
+    public ExpressionFunctionAlias first() {
+        return this.pluginAliasSet.first();
+    }
+
+    @Override
+    public ExpressionFunctionAlias last() {
+        return this.pluginAliasSet.last();
+    }
+
+    @Override
+    public String text() {
         return this.pluginAliasSet.text();
+    }
+
+    @Override
+    public void printTree(final IndentingPrinter printer) {
+        this.pluginAliasSet.printTree(printer);
     }
 
     private final PluginAliasSet<ExpressionFunctionName, ExpressionFunctionInfo, ExpressionFunctionInfoSet, ExpressionFunctionSelector, ExpressionFunctionAlias> pluginAliasSet;
@@ -115,5 +202,4 @@ public final class ExpressionFunctionAliases implements PluginAliasesLike<Expres
         );
         ExpressionFunctionInfoSet.EMPTY.size(); // trigger static init and json marshall/unmarshall registry
     }
-
 }
